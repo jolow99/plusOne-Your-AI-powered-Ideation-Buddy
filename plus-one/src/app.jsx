37ebc25af108f35ad470d704ae58d73c7b66faf2
Jolow <jolow999@gmail.com>
2022-07-18 10:27:34 +0800

    Write text object and overflow frames

--- plus-one/src/app.jsx
+++ plus-one/src/app.jsx
@@ -10,41 +10,86 @@ function App() {
   let [loadingResults, setLoadingResults] = React.useState(false)
 
   async function createStickies(clusters, labels) {
+    let create_start = new Date().getTime()
+    let frame_x = 0
+    let frame_y = 0
+    let cluster;
+    let label;
+    let sticky_x;
+    let sticky_y;
+    let frame;
+    let text_label;
+    let note;
+    let sticky;
     // loop through count of clusters and labels
     setLoadingResults(true)
     for (let i = 0; i < clusters.length; i++) {
-      let cluster = clusters[i]
-      let label = labels[i]
-      let rel_x = 0
-      let rel_y = 0
-      let frame = await miro.board.createFrame({
-        title: label,
+      cluster = clusters[i]
+      label = labels[i]
+      sticky_x = 0
+      sticky_y = 0
+      frame = await miro.board.createFrame({
+        title: "Cluster" + i,
         width: 8000,
         height: 4500,
       })
       console.log("creating frame" + i)
+      text_label = await miro.board.createText({
+        content: label,
+        style: {
+          fillColor: 'transparent', // Default value: transparent (no fill)
+          fillOpacity: 1, // Default value: 1 (solid color)
+          fontFamily: 'roboto', // Default font type for the text
+          fontSize: 150, 
+          textAlign: 'left', // Default alignment: left
+        },
+        width: 4000,
+        x: sticky_x+4000,
+        y: sticky_y+250,
+       })
+       await frame.add(text_label)
+      
+
       // loop through index of cluster and create sticky notes
       for (let j = 0; j < cluster.length; j++) {
-        let note = cluster[j]
+        note = cluster[j]
         if (j%5 == 0) {
-          rel_x = 0
-          rel_y += 600
+          if (j != 0) {
+            sticky_x = 0
+            sticky_y += 600
+            console.log("Moving sticky down by 600")
+          }
+          console.log("Not moving sticky")
         } else {
-          rel_x += 600
+          sticky_x += 600
+          console.log("Moving sticky right by 600")
         }
-        const sticky = await miro.board.createStickyNote({
+        sticky = await miro.board.createStickyNote({
           content: note,
-          x: rel_x,
-          y: rel_y,
+          x: sticky_x+500,
+          y: sticky_y+1000,
           shape: 'square',
           width: 500
         })
         await frame.add(sticky)
       }
-      frame.x = 10000*i
-      frame.y = 0
+      if (i%5 == 0) {
+        if (i != 0) {
+          frame_x = 0
+          frame_y += 5000
+          console.log("Moving frame down by 5000")
+        }
+        console.log("Not moving frame")
+      } else {
+        frame_x += 10000
+        console.log("Moving frame right by 10000")
+      }
+      frame.x = frame_x
+      frame.y = frame_y
       await frame.sync()
     }
+    let create_end = new Date().getTime()
+    console.log("Time taken for Writing: " + (create_end - create_start) + "ms") 
     console.log("Created stickies!")
     setLoadingResults(false)
   }
@@ -56,6 +101,7 @@ function App() {
   }
 
   async function generate() {
+    let api_start = new Date().getTime()
     setLoadingAPI(true)
     let selectedItems = await miro.board.getSelection()
     let stickies = selectedItems.filter(item => item.type === 'sticky_note')
@@ -77,6 +123,8 @@ function App() {
       console.log("clusters:" + clusters)
       console.log("labels:" + labels)
       setLoadingAPI(false)
+      let api_end = new Date().getTime()
+      console.log("Time taken for API: " + (api_end - api_start) + "ms") 
       createStickies(clusters, labels)
     })
     .catch(function (error) {
